9aed7255ee7e000218b6c6be4ad1971f74dc69e1
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-03 20:22:18 +0530

    Add selection view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DataTablesListComponent } from './data-tables-list/data-tables-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CellsViewComponent } from './cells-view/cells-view.component';
+import { SelectionViewComponent } from './selection-view/selection-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataTablesListComponent,
     NavbarComponent,
-    CellsViewComponent
+    CellsViewComponent,
+    SelectionViewComponent
   ],
   imports: [
     BrowserModule,
