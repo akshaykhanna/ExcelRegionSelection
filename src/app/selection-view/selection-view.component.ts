75ad6dfdd36d810ad5e270a1f4093d3cd6c5dd38
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-04 07:05:43 +0530

    Toggle selection view based on cell selected or not

--- src/app/selection-view/selection-view.component.ts
+++ src/app/selection-view/selection-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-selection-view',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selection-view.component.css']
 })
 export class SelectionViewComponent implements OnInit {
-
+  @Input() selectedCell = '';
+  isCellSelected: boolean;
   constructor() { }
 
   ngOnInit() {
+    this.isCellSelected = this.selectedCell !== '';
   }
 
 }
