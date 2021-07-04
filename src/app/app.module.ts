a3b801c7c66ae3d76a48a1f4d6984a72aa41e892
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-04 16:05:40 +0530

    Add cells-grid component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CellsViewComponent } from './cells-view/cells-view.component';
 import { SelectionViewComponent } from './selection-view/selection-view.component';
 import { MarkedCellsViewComponent } from './marked-cells-view/marked-cells-view.component';
+import { CellsGridComponent } from './cells-grid/cells-grid.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MarkedCellsViewComponent } from './marked-cells-view/marked-cells-view.
     NavbarComponent,
     CellsViewComponent,
     SelectionViewComponent,
-    MarkedCellsViewComponent
+    MarkedCellsViewComponent,
+    CellsGridComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cells-grid/cells-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cells-grid',
+  templateUrl: './cells-grid.component.html',
+  styleUrls: ['./cells-grid.component.css']
+})
+export class CellsGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
