7d21dea1f6fb43bcf3d7610f895856435ded9d02
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-04 19:08:24 +0530

    Refractor UI of cell-view, marked-cell-view & tables-list-view

--- src/app/cells/marked-cells-view/marked-cells-view.component.ts
+++ src/app/cells/marked-cells-view/marked-cells-view.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-marked-cells-view',
   templateUrl: './marked-cells-view.component.html',
-  styleUrls: ['./marked-cells-view.component.css']
+  styleUrls: ['./marked-cells-view.component.css'],
 })
 export class MarkedCellsViewComponent implements OnInit {
-  markedCells = ['E1', 'E2', 'E3'];
-  constructor() { }
+  // @Input() markedCells = [];
+  @Input() markedCells = ['E1', 'E2', 'E3'];
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
