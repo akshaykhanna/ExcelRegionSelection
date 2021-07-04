4d5a7472241c50340056070342544cdbf5cd73e5
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-04 08:25:48 +0530

    Fix table list view

--- src/app/tables-list-view/tables-list-view.component.ts
+++ src/app/tables-list-view/tables-list-view.component.ts
@@ -3,17 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tables-list-view',
   templateUrl: './tables-list-view.component.html',
-  styleUrls: ['./tables-list-view.component.css']
+  styleUrls: ['./tables-list-view.component.css'],
 })
 export class DataTablesListComponent implements OnInit {
-  dataTablesString = [
-    'Data table 1',
-    'Data table 2',
-    'Data table 3',
- ]
-  constructor() { }
+  dataTablesString = ['Data table 1', 'Data table 2', 'Data table 3'];
+  seletedTable: string;
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  onTableSelect(tableName) {
+    this.seletedTable = tableName;
   }
-
+  isTableSelected(tableName) {
+    return this.seletedTable === tableName;
+  }
 }
