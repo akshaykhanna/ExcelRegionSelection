db7de3fae91d10642e961946e86307826d1a4ed4
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-08 12:40:59 +0530

    Fix cell-view-component test

--- src/app/cells/cells-view/cells-view.component.spec.ts
+++ src/app/cells/cells-view/cells-view.component.spec.ts
@@ -1,16 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CellsViewComponent } from './cells-view.component';
+import { CellsGridComponent } from '../cells-grid/cells-grid.component';
+import { SelectionViewComponent } from '../selection-view/selection-view.component';
+import { MarkedCellsViewComponent } from '../marked-cells-view/marked-cells-view.component';
+import { CellsService } from '../cells.service';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
 
 describe('CellsViewComponent', () => {
   let component: CellsViewComponent;
   let fixture: ComponentFixture<CellsViewComponent>;
-
+  const cellsServiceStub: Partial<CellsService> = {
+    getCellsData: () => new Observable(),
+    colName: () => {
+      return 'A1';
+    },
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CellsViewComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        CellsViewComponent,
+        CellsGridComponent,
+        SelectionViewComponent,
+        MarkedCellsViewComponent,
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { paramMap: convertToParamMap({ uuid: 'dummy' }) },
+          },
+        },
+        { provide: CellsService, useValue: cellsServiceStub },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
