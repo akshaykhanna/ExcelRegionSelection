df0d86c617037368f5c5ab354096a65c5d9992d1
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-08 11:21:40 +0530

    Fix tables-list-view test

--- src/app/tables-list-view/tables-list-view.component.spec.ts
+++ src/app/tables-list-view/tables-list-view.component.spec.ts
@@ -1,16 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DataTablesListComponent } from './tables-list-view.component';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
 
 describe('DataTablesListComponent', () => {
   let component: DataTablesListComponent;
   let fixture: ComponentFixture<DataTablesListComponent>;
-
+  const routerStub: Partial<Router> = {
+    navigate: () => {
+      return new Promise<boolean>(null);
+    },
+  };
+  const dataServiceStub: Partial<DataService> = {
+    getDataTables: () => {
+      return new Observable();
+    },
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DataTablesListComponent ]
-    })
-    .compileComponents();
+      declarations: [DataTablesListComponent],
+      providers: [
+        { provide: Router, useValue: routerStub },
+        { provide: DataService, useValue: dataServiceStub },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
