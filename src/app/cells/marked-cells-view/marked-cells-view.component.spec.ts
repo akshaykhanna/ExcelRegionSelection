0e525fbb9f385edc4c73f9e9f5933e939af70349
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-09 00:00:57 +0530

    Add test for marked-cells-view

--- src/app/cells/marked-cells-view/marked-cells-view.component.spec.ts
+++ src/app/cells/marked-cells-view/marked-cells-view.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MarkedCellsViewComponent } from './marked-cells-view.component';
+import Cell from '../../models/cell';
+import { CellStatus } from 'src/app/models/cell';
 
 describe('MarkedCellsViewComponent', () => {
   let component: MarkedCellsViewComponent;
@@ -21,5 +23,14 @@ describe('MarkedCellsViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.markedCells.length).toBe(0);
   });
+
+  it('should unMarkCell', () => {
+    const cell = new Cell('A', 1, 'Test', CellStatus.MARKED);
+    expect(cell.status).toBe(CellStatus.MARKED);
+    component.unMarkCell(cell)
+    expect(cell.status).toBe(CellStatus.NORMAL);
+  });
+
 });
