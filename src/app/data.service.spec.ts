d00996f46837e1cd0b4c004edadf98a238fdb588
akshay.khanna <akshay.khanna@thoughtworks.com>
2021-07-08 23:54:29 +0530

    Add test for dataService

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -3,16 +3,27 @@ import { TestBed } from '@angular/core/testing';
 import { DataService } from './data.service';
 import { HttpService } from './http.service';
 import { httpServiceStub } from './stubs';
+import { of } from 'rxjs';
 
 describe('DataService', () => {
-  beforeEach(() =>
+  let service: DataService;
+  let httpService: HttpService;
+  beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [{ provide: HttpService, useValue: httpServiceStub }],
-    })
+    });
+    service = TestBed.get(DataService);
+    httpService = TestBed.get(HttpService);
+  }
   );
 
   it('should be created', () => {
-    const service: DataService = TestBed.get(DataService);
     expect(service).toBeTruthy();
+  });
+
+  it('getDataTables should make graphQLRequest ', async () => {
+    const httpGQLSpy = spyOn(httpService, 'graphQLRequest').and.returnValue(of([]));
+    await service.getDataTables();
+    expect(httpGQLSpy).toHaveBeenCalled();
   });
 });
